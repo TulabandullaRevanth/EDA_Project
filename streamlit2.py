8ec5b1534b6d6cbf612b95985677c2153ab37930
TulabandullaRevanth <tulabandullarevanth@gmail.com>
2025-10-21 17:47:56 +0530

    new

--- streamlit2.py
+++ streamlit2.py
@@ -787,14 +787,14 @@ elif page == "🗳️ Gender Analysis":
     else:
         # Normalize gender values
         df_filtered["Gender"] = df_filtered["sex"].replace({
-         "M": "Male",
-         "F": "Female",
-         "O": "Other",
-         "o": "Other",
-         "THIRD": "Third",
-         "Third": "Third",
-         "FEMALE": "Female",
-         "MALE": "Male"
+           "M": "Male",
+           "F": "Female",
+           "O": "Other",
+           "o": "Other",
+           "THIRD": "Third",
+           "Third": "Third",
+           "FEMALE": "Female",
+           "MALE": "Male"
 })
 
         # Aggregate votes by gender
